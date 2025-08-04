70fd4a9c7207f85c92220d12383433ff205f5bde
Biswadip125 <biswadipchowdhury114@gmail.com>
2025-08-04 10:31:30 +0530

    feat: setup static file serving and production build routing for frontend

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,12 @@ import cookieParser from "cookie-parser";
 import postsRouter from "./routes/post.routes.js";
 import userRouter from "./routes/user.route.js";
 import cors from "cors";
+import path from "path";
 dotenv.config();
 
 const app = express();
+
+const _dirname = path.resolve();
 
 //middlewares
 app.use(express.json());
@@ -25,6 +28,12 @@ app.use(
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/posts", postsRouter);
 app.use("/api/v1/users", userRouter);
+
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+console.log(_dirname);
+app.get("/*splat", (_, res) => {
+  res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(process.env.PORT, async () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useUser } from "../../context/UserContext";
 import toast from "react-hot-toast";
 import axios from "axios";
